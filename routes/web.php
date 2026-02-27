d9c6f08c901d70a4c0c9027bfc752cbac7f34f02
Anton Kristiono <antonkristiono47@gmail.com>
2026-02-27 15:41:31 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
+use App\Http\Controllers\Task\Controller as TaskController;
 
 Route::inertia('/', 'Welcome', [
     'canRegister' => Features::enabled(Features::registration()),
@@ -10,5 +11,14 @@ Route::inertia('/', 'Welcome', [
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
 });
+
+// make route for task
+Route::get('/task', [TaskController::class, 'index'])->name('task.index');
+Route::get('', [TaskController::class, 'create'])->name('task.create');
+Route::post('', [TaskController::class, 'store'])->name('task.store');
+Route::get('/{id}', [TaskController::class, 'show'])->name('task.show');
+Route::put('/{id}', [TaskController::class, 'update'])->name('task.update');
+Route::delete('/{id}', [TaskController::class, 'destroy'])->name('task.destroy');
+
 
 require __DIR__.'/settings.php';
